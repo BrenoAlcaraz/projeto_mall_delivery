00f35e348e152db78d35feaa2a25919adc89b116
BrenoFrança <brenofafrj3@gmail.com>
2024-05-17 15:08:37 -0300

    .gitignore

--- projeto_mall_delivery/app_cadastro/urls.py
+++ projeto_mall_delivery/app_cadastro/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('cadastro/cadastroconsumidor/', views.consumidor_cadastro, name='consumidor_cadastro'),
     path('cadastro/cadastrolojista/', views.lojista_cadastro, name='lojista_cadastro'),
     path('login/', views.pagina_login, name='pagina_login'),
-    path('', views.pagina_inicial, name='pagina_inicial'),
+    ##path('', views.pagina_inicial, name='pagina_inicial'),
+    path('cadastro/lojista_inicial/',views.lojista_inicial,name='lojista_inicial')
 ]
